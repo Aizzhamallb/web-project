071f4ed68ff4de957b1285fd3ca7ce208a2ededc
Aizhamal <aizhamal.batyrbekova@alatoo.edu.kg>
2023-03-23 16:31:08 +0600

    admin

--- main/admin.py
+++ main/admin.py
@@ -3,6 +3,4 @@ from django.contrib import admin
 from main.models import Category, Post, Comment
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Post)
-admin.site.register(Comment)
+
